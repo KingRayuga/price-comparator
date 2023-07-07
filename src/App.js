a280c0cddffdab41a426b9ca7079ed061bd9f57b
KingRayuga <yashraj1605@gmail.com>
2023-07-08 01:43:20 +0530

    Add: Header,Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const Applayout = () => {
     return (
         <div className="app">
-            <Header />
+            <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('https://assets0.ello.co/uploads/asset/attachment/2258444/ello-optimized-c13af33c.gif')]">
+                <Header />
+            </div>
+            <Footer/>
         </div>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import React from "react"
 
 const Header = () => {
     return(
-        <>
-        </>
+        <div className="card card-compact w-[95%] h-20 bg-base-100 shadow-xl m-auto mt-0.5 image-full">
+        <figure><img className="w-full h-full object-cover" src="https://c4.wallpaperflare.com/wallpaper/553/119/620/windows-10-windows-10x-windows-11-minimalism-material-minimal-hd-wallpaper-preview.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+            <h1 className="card-title items-center justify-center text-4xl">Price comparator!</h1>
+        </div>
+        </div>
+     
     )};
 
 export default Header;

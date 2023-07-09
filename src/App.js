8bf810fcd044860999671a5e12aa2a12f8f73fa8
KingRayuga <yashraj1605@gmail.com>
2023-07-09 22:18:46 +0530

    Add: Button,Search-Bar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Body from "./components/Body";
 const Applayout = () => {
     return (
         <div className="app">
-            <div className="min-h-screen bg-cover bg-center bg-[url('https://assets0.ello.co/uploads/asset/attachment/2258444/ello-optimized-c13af33c.gif')]">
+            <div className="min-h-screen bg-cover bg-center bg-[url('https://cdn.dribbble.com/users/9453/screenshots/4273970/polybelt.gif')]">
             <div className="flex items-center justify-center">
                 <Header />
             </div>
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Body = () =>{
     return(
-        <div class="flex w-full">
-        <div class="grid h-screen flex-grow card bg-base-300 rounded-box place-items-center m-6">content</div>
-        <div class="grid h-screen flex-grow card bg-base-300 rounded-box place-items-center m-6">content</div>
+        <div className="flex w-full">
+        <div className="grid grid-rows-6 gap-0 h-screen w-8/12 flex-grow card bg-base-300 rounded-box place-items-center m-6 ">
+            <div className="join search-bar w-1/2">
+                <input className="input input-bordered join-item w-3/4" placeholder="Search"/>
+                <button className="btn join-item w-1/4 btn-accent">Search</button>
+            </div>
+        </div>
+        <div className="grid h-screen w-4/12 flex-grow card bg-base-300 rounded-box place-items-center m-6">
+            content
+        </div>
         </div>
     )
 }
